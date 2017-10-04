0fd0d37fa270203f5099718826ad90788330a7e8
Darren Gilroy <gilroy@gmail.com>
2017-10-03 19:20:04 -0700

    Update w/ UTi & Cedexis

--- code.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $("#header").load("header.html");
-  $("#footer").load("footer.html");
-  $("span.myemail").load("email.txt");
-});
-
--- init.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $("#header").addClass("header block");
-  $("#header").load("header.html");
-  $("#footer").load("footer.html");
-  $("span.myemail").load("email.txt");
-});
-
